fd5f0bf3a09c21925890adadb8a93b2a48b4a559
naybala <naybalaismylife.nbl@gmail.com>
2022-02-14 16:21:05 +0630

    adding responsive and others

--- app.js
+++ app.js
@@ -1,1 +1,9 @@
-//Cooming Soon//
+
+
+const navToggle = document.querySelector(".nav-toggle");
+const links = document.querySelector(".links");
+
+navToggle.addEventListener("click", function () {
+
+    links.classList.toggle("show-links");
+});
